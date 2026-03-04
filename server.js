1bfe42c8b01229eaf9da06f9598d752c73ff79a3
Verma <ashutoshverma@alvarezandmarsal.com>
2026-03-04 23:09:03 +0530

    update

--- server.js
+++ server.js
@@ -10,15 +10,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.post("/chat", async (req, res) => {
-
-  const userMessage = req.body.message;
-
   try {
     const response = await axios.post(
       "https://api.cohere.ai/v1/chat",
       {
-        model: "command-r-08-2024",
-        message: userMessage
+        message: req.body.message,
+        model: "command-r"
       },
       {
         headers: {
@@ -28,13 +25,14 @@ app.post("/chat", async (req, res) => {
       }
     );
 
-    res.json({
-      reply: response.data.text
-    });
+    res.json({ reply: response.data.text });
 
   } catch (error) {
-    console.log(error.response?.data);
-    res.status(500).send("Error calling Cohere");
+    console.error(error.response?.data || error.message);
+
+    res.status(500).json({
+      error: "Error calling Cohere"
+    });
   }
 });
 const PORT = process.env.PORT || 3000;
